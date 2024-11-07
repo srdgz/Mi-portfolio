c1e8a464d2b15a21ce95718477bf8f64295b430f
srdgz <sandra.rr@icloud.com>
2024-11-07 16:47:41 +0100

    fix: modify experience and education in about page

--- mi-portfolio/src/views/About.jsx
+++ mi-portfolio/src/views/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
   return (
     <div className="max-w-md md:max-w-4xl xl:max-w-7xl mx-auto mt-8 md:mt-18 p-6">
       <Heading>Sobre mí</Heading>
-      <p className="text-sm md:text-lg text-gray-500 mb-4">
+      <p className="text-sm md:text-lg text-gray-500 mb-4 lg:mx-8">
         He dado un giro a mi carrera profesional hacia el desarrollo web,
         aprovechando habilidades clave como la atención al detalle y la
         rigurosidad, esenciales en este campo. Desde entonces, he trabajado en
@@ -26,7 +26,7 @@ const About = () => {
         atractivos y tecnológicamente avanzados, características fundamentales
         en el desarrollo de software moderno.
       </p>
-      <p className="text-sm md:text-lg text-gray-500 mb-8">
+      <p className="text-sm md:text-lg text-gray-500 mb-8 lg:mx-8">
         Me enfoco en seguir aprendiendo y en colaborar eficazmente en equipo,
         manteniendo siempre una actitud proactiva. Si buscas a alguien que
         aporte una perspectiva fresca y actual para tu próximo proyecto, no
@@ -34,7 +34,7 @@ const About = () => {
         proyecto.
       </p>
       <Heading>Stack</Heading>
-      <ul className="grid grid-cols-6 lg:grid-cols-12 gap-4 md:gap-6 justify-center items-center list-none text-sm md:text-lg text-gray-500 mb-8">
+      <ul className="grid grid-cols-6 lg:grid-cols-12 gap-4 md:gap-6 justify-center items-center list-none text-sm md:text-lg text-gray-500 mb-8 lg:mx-8">
         <li className="flex items-center relative group">
           <img src={javascript} alt="JavaScript" className="h-8 md:h-10" />
           <span className="absolute hidden group-hover:block text-gray-600 bg-green-200 rounded-[50px] px-2 py-1 text-sm top-full left-1/2 transform -translate-x-1/2 mt-2 z-10">
@@ -109,11 +109,16 @@ const About = () => {
         </li>
       </ul>
       <Heading>Experiencia</Heading>
-      <ul className="list-none text-start text-sm md:text-lg text-gray-500 mb-4">
-        <li className="mb-4">
-          <strong>Full-Stack Developer en Bubbo</strong> · Enero - Septiembre
-          2024
-          <p>
+      <ol className="relative border-s border-blue-400 lg:mx-8">
+        <li className="mb-10 ms-4">
+          <div className="absolute w-3 h-3 bg-blue-400 rounded-full mt-1.5 -start-1.5"></div>
+          <time className="mb-1 text-md font-normal leading-none text-gray-500">
+            Enero - Septiembre 2024
+          </time>
+          <h3 className="text-lg font-semibold text-gray-500 my-2">
+            Desarrolladora Full-Stack en Bubbo
+          </h3>
+          <p className="mb-4 text-base font-normal text-gray-500">
             Desarrollo de la aplicación móvil Bubbo TV con React Native,
             TypeScript y Expo tanto para iOS como Android, con el fin de ofrecer
             una experiencia de usuario fluida y atractiva.
@@ -137,10 +142,15 @@ const About = () => {
             código.
           </p>
         </li>
-        <li className="mb-8">
-          <strong>Full-Stack Developer en Clidefit</strong> · Septiembre -
-          Diciembre 2023
-          <p>
+        <li className="mb-10 ms-4">
+          <div className="absolute w-3 h-3 bg-blue-400 rounded-full mt-1.5 -start-1.5"></div>
+          <time className="mb-1 text-md font-normal leading-none text-gray-500">
+            Septiembre - Diciembre 2023
+          </time>
+          <h3 className="text-lg font-semibold text-gray-500 my-2">
+            Desarrolladora Full-Stack en Clidefit
+          </h3>
+          <p className="text-base font-normal text-gray-500">
             Desarrollo y mantenimiento de website utilizando tecnologías
             modernas como React y Tailwind CSS para crear un diseño limpio y
             moderno.
@@ -157,37 +167,81 @@ const About = () => {
             simplificar la experiencia de usuario.
           </p>
         </li>
-      </ul>
+        <li className="ms-4">
+          <div className="absolute w-3 h-3 bg-blue-400 rounded-full mt-1.5 -start-1.5"></div>
+          <time className="mb-1 text-md font-normal leading-none text-gray-500">
+            Junio - Agosto 2023
+          </time>
+          <h3 className="text-lg font-semibold text-gray-500 my-2">
+            Desarrolladora Full-Stack en RoomieConnect (Proyecto colaborativo)
+          </h3>
+          <p className="text-base font-normal text-gray-500">
+            Coordiné un equipo de 3 desarrolladores para crear RoomieConnect,
+            una plataforma web para gestionar tareas y gastos compartidos entre
+            compañeros de piso.
+            <br />
+            Desarrollo del frontend utilizando React y Tailwind CSS para
+            proporcionar una interfaz de usuario intuitiva y responsiva.
+            <br />
+            Backend desarrollado en Python con Flask y PostgreSQL, gestionando
+            la arquitectura y las APIs que permitían el registro de tareas y
+            pagos.
+            <br />
+            Integración de APIs externas como Cloudinary para el manejo de
+            imágenes y PayPal para la gestión de pagos.
+          </p>
+        </li>
+      </ol>
       <Heading>Formación</Heading>
-      <ul className="list-none text-end text-sm md:text-lg text-gray-500 mb-8">
-        <li className="mb-4">
-          <strong>
+      <ol className="relative border-s border-blue-400 lg:mx-8">
+        <li className="mb-10 ms-4">
+          <div className="absolute w-3 h-3 bg-blue-400 rounded-full mt-1.5 -start-1.5"></div>
+          <time className="mb-1 text-md font-normal leading-none text-gray-500">
+            Cursando
+          </time>
+          <h3 className="text-lg font-semibold text-gray-500 my-2">
             Ciclo superior en desarrollo de aplicaciones web (DAW)
-          </strong>
-          <p>I.E.S. de Teis</p>
-          <p>Cursando</p>
+          </h3>
+          <p className="mb-4 text-base font-normal text-gray-500">
+            I.E.S. de Teis
+          </p>
         </li>
-        <li className="mb-4">
-          <strong>Full Stack Software Development</strong>
-          <p>4Geeks Academy</p>
-          <p>2023</p>
+        <li className="mb-10 ms-4">
+          <div className="absolute w-3 h-3 bg-blue-400 rounded-full mt-1.5 -start-1.5"></div>
+          <time className="mb-1 text-md font-normal leading-none text-gray-500">
+            2023
+          </time>
+          <h3 className="text-lg font-semibold text-gray-500 my-2">
+            Full Stack Software Development
+          </h3>
+          <p className="text-base font-normal text-gray-500">4Geeks Academy</p>
         </li>
-        <li className="mb-4">
-          <strong>
+        <li className="mb-10 ms-4">
+          <div className="absolute w-3 h-3 bg-blue-400 rounded-full mt-1.5 -start-1.5"></div>
+          <time className="mb-1 text-md font-normal leading-none text-gray-500">
+            2011
+          </time>
+          <h3 className="text-lg font-semibold text-gray-500 my-2">
             Máster en formación del profesorado en E.S.O. y Bachillerato
-          </strong>
-          <p>Universidad de Extremadura</p>
-          <p>2011</p>
+          </h3>
+          <p className="text-base font-normal text-gray-500">
+            Universidad de Extremadura
+          </p>
         </li>
-        <li className="mb-4">
-          <strong>
+        <li className="ms-4">
+          <div className="absolute w-3 h-3 bg-blue-400 rounded-full mt-1.5 -start-1.5"></div>
+          <time className="mb-1 text-md font-normal leading-none text-gray-500">
+            2008
+          </time>
+          <h3 className="text-lg font-semibold text-gray-500 my-2">
             Licenciatura en Historia, Historia Antigua y Arqueología
-          </strong>
-          <p>Universidad Autónoma de Madrid</p>
-          <p>2008</p>
+          </h3>
+          <p className="text-base font-normal text-gray-500">
+            Universidad Autónoma de Madrid
+          </p>
         </li>
-      </ul>
-      <div className="flex justify-end mt-20 mb-20">
+      </ol>
+      <div className="flex justify-center mt-20 mb-20">
         <DownloadCvButton />
       </div>
     </div>
