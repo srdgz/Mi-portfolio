c0a4de3ad3a55ad9ff4420885d33e8514e7aeb23
srdgz <sandra.rr@icloud.com>
2024-11-07 15:42:13 +0100

    fix: modify buttons with link to anchor

--- mi-portfolio/src/components/DemoButton.jsx
+++ mi-portfolio/src/components/DemoButton.jsx
@@ -1,13 +1,15 @@
 const DemoButton = ({ demoLink }) => {
   return (
     <div className="flex justify-end">
-      <button
+      <a
+        href={demoLink}
+        target="_blank"
+        rel="noopener noreferrer"
         className="p-2 bg-blue-500 text-white rounded-[50px] hover:bg-blue-600 focus:outline-none shadow-lg"
-        onClick={() => window.open(demoLink, "_blank", "noopener noreferrer")}
         aria-label="Abrir la demo del proyecto"
       >
         <i className="fa-solid fa-globe fa-xl"></i>
-      </button>
+      </a>
     </div>
   );
 };
--- mi-portfolio/src/components/RepoButton.jsx
+++ mi-portfolio/src/components/RepoButton.jsx
@@ -1,13 +1,15 @@
 const RepoButton = ({ repoLink }) => {
   return (
     <div className="flex justify-end">
-      <button
+      <a
+        href={repoLink}
+        target="_blank"
+        rel="noopener noreferrer"
         className="p-2 bg-blue-500 text-white rounded-[50px] hover:bg-blue-600 focus:outline-none shadow-lg"
-        onClick={() => window.open(repoLink, "_blank", "noopener noreferrer")}
         aria-label="Abrir el repositorio del proyecto"
       >
         <i className="fa-brands fa-github fa-xl"></i>
-      </button>
+      </a>
     </div>
   );
 };
